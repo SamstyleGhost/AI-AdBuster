c812963f31b153f794e16b850ded2e27e027b963
SamstyleGhost <rohancrrm@gmail.com>
2025-01-20 01:52:43 +0530

    Working spooky descriptions

--- src/content.ts
+++ src/content.ts
@@ -1,78 +1,157 @@
-// import './index.css'
-//
-//
-// setTimeout(() => {
-//   const elements = document.querySelectorAll('div[id^="google_ads_iframe"]')
-//   console.log(elements)
-//   
-//   for (let index = 0; index < elements.length; index++) {
-//     const element = elements[index];
-//     element.outerHTML = "Hello from this side"
-//   }
-// }, 5000)
-// 
+import { getAdDescription } from "./utils/utils";
 
-import { checkAds } from "./utils/utils";
 
-const potentialAds = new Set<string>();
+const data : string[] = [
+  'div[data-google-query-id]:not([data-replaced="true"])',
+  'div[id^="google_ads_iframe"]:not([data-replaced="true"])',
+  'div[id^="_vdo_ads_player_ai"]:not([data-replaced="true"])',
+  'iframe[aria-label="Advertisement"]:not([data-replaced="true"])'
+]
 
-// Monitor DOM changes
-const observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => {
-          if (node.nodeType === Node.ELEMENT_NODE) { // Ensure it's an element node
-              const tagName = node.tagName.toLowerCase();
-              if (tagName === 'div' || tagName === 'script') {
-                  const nodeId = node.id ? node.id : '';
-                  if(nodeId !== '') {
-                    potentialAds.add(nodeId)
-                  }
-              }
+
+const replaceAds = () => {
+  const selectors = data.join(',');
+  const elements = document.querySelectorAll(selectors);
+
+  elements.forEach((element) => {
+    element.setAttribute('data-replaced', 'true');
+
+    const replacementContent = document.createElement('div');
+    replacementContent.innerHTML = `
+      <i class="icon-class">🔔</i>
+      <span>Loading description...</span>
+    `;
+
+    let adDescription = "Sorry! no description";
+
+    if (element.tagName !== "IFRAME") {
+      const iframe = element.querySelector('iframe');
+      if (iframe) {
+        try {
+          const iframeDoc = iframe?.contentDocument || iframe?.contentWindow?.document;
+          const iframeImage = iframeDoc?.querySelector('img');
+
+          if (iframeImage) {
+            console.log('Image found inside iframe with src:', iframeImage.src);
+            getAdDescription(iframeImage.src)
+              .then((res) => {
+                console.log("Ad description is:", res);
+                adDescription = res;
+
+                replacementContent.innerHTML = `
+                  <i class="icon-class">🔔</i>
+                  <span>${adDescription}</span>
+                `;
+              })
+              .catch((err) => {
+                console.error('Error fetching ad description:', err);
+                replacementContent.innerHTML = `
+                  <i class="icon-class">🔔</i>
+                  <span>Sorry! no description</span>
+                `;
+              });
           }
-      });
-  });
-  console.log("Potential Ads are: ", potentialAds)
-});
-observer.observe(document.body, { childList: true, subtree: true });
+        } catch (error) {
+          console.error('Error accessing iframe content:', error);
+          replacementContent.innerHTML = `
+            <i class="icon-class">🔔</i>
+            <span>Sorry! no description</span>
+          `;
+        }
+      }
+    }
 
-const callGroq = async () => {
-  const idsArray: string[] = Array.from(potentialAds)
-  const response = await checkAds(idsArray)
-  const JSONresponse = await JSON.parse(response?.content)
-  console.log(JSONresponse)
+    const imageEle = element.querySelector('img');
+    if (imageEle) {
+      console.log(imageEle.src);
+    } else {
+      replacementContent.innerHTML = `
+        <i class="icon-class">🔔</i>
+        <span>Sorry! no description</span>
+      `;
+    }
+
+    element.replaceWith(replacementContent);
+  });
+};
+
+const removeAds = () => {
+  const selectors = data.join(',')
+  const elements = document.querySelectorAll(selectors);
+  elements.forEach(element => {
+    element.remove()
+  })
 }
 
-window.addEventListener("load", () => [
-  callGroq()
-])
+const observer = new MutationObserver(replaceAds);
+observer.observe(document.body, { childList: true, subtree: true });
 
-// Intercept Fetch and XHR
-const originalFetch = window.fetch;
-window.fetch = async (...args) => {
-  console.log(`Fetch Request: ${args[0]}`);
-  const response = await originalFetch(...args);
-  const clone = response.clone();
-  clone.text().then(text => console.log(`Response from ${args[0]}:`, text));
-  return response;
-};
-
-const originalXHR = XMLHttpRequest.prototype.open;
-XMLHttpRequest.prototype.open = function (method, url) {
-  console.log(`XHR Request: ${method} ${url}`);
-  this.addEventListener('load', function () {
-      console.log(`Response from ${url}:`, this.responseText);
-  });
-  return originalXHR.apply(this, arguments);
-};
-
-// Override DOM modification methods
-const originalInnerHTML = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML');
-Object.defineProperty(Element.prototype, 'innerHTML', {
-  set(value) {
-      console.log('innerHTML set on', this, 'with value:', value);
-      originalInnerHTML.set.call(this, value);
-  },
-  get() {
-      return originalInnerHTML.get.call(this);
-  }
-});
+// import { checkAds } from "./utils/utils";
+//
+// const potentialAds = new Set<string>();
+//
+// // Monitor DOM changes
+// const observer = new MutationObserver(mutations => {
+//   mutations.forEach(mutation => {
+//       mutation.addedNodes.forEach(node => {
+//           if (node.nodeType === Node.ELEMENT_NODE) { // Ensure it's an element node
+//               const tagName = node.tagName.toLowerCase();
+//               if (tagName === 'div' || tagName === 'script') {
+//                   const nodeId = node.id ? node.id : '';
+//                   if(nodeId !== '') {
+//                     potentialAds.add(nodeId)
+//                   }
+//               }
+//           }
+//       });
+//   });
+//   console.log("Potential Ads are: ", potentialAds)
+// });
+// observer.observe(document.body, { childList: true, subtree: true });
+//
+// const callGroq = async () => {
+//   const idsArray: string[] = Array.from(potentialAds)
+//   const response = await checkAds(idsArray)
+//   const JSONresponse : string[] = await JSON.parse(response?.content)
+//
+//   JSONresponse.map(ad => {
+//     document.querySelector()
+//   }) 
+//
+//   console.log(JSONresponse)
+// }
+//
+// window.addEventListener("load", () => [
+//   callGroq()
+// ])
+//
+// // Intercept Fetch and XHR
+// const originalFetch = window.fetch;
+// window.fetch = async (...args) => {
+//   console.log(`Fetch Request: ${args[0]}`);
+//   const response = await originalFetch(...args);
+//   const clone = response.clone();
+//   clone.text().then(text => console.log(`Response from ${args[0]}:`, text));
+//   return response;
+// };
+//
+// const originalXHR = XMLHttpRequest.prototype.open;
+// XMLHttpRequest.prototype.open = function (method, url) {
+//   console.log(`XHR Request: ${method} ${url}`);
+//   this.addEventListener('load', function () {
+//       console.log(`Response from ${url}:`, this.responseText);
+//   });
+//   return originalXHR.apply(this, arguments);
+// };
+//
+// // Override DOM modification methods
+// const originalInnerHTML = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML');
+// Object.defineProperty(Element.prototype, 'innerHTML', {
+//   set(value) {
+//       console.log('innerHTML set on', this, 'with value:', value);
+//       originalInnerHTML.set.call(this, value);
+//   },
+//   get() {
+//       return originalInnerHTML.get.call(this);
+//   }
+// });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,7 +6,7 @@ export async function checkAds(ids : string[]) {
   const prompt = `You are a web assistant that can tell whether a HTML element is a part of an advertisement. You are given an array of HTML div's id fields here:\n
   ${ids}\n
 
-  What I want you to return is an array of the id's that are part of an advertisement service. Only return the array and no extra content. Do not explain or add any other text. Just the array.
+  What I want you to return is an array of the id's that are part of an advertisement service. Only return the array and no extra content. Do not explain or add any other text. Just return an array of strings.
   `
 
   try {
@@ -29,4 +29,28 @@ export async function checkAds(ids : string[]) {
   } catch (error) {
     console.error(error)
   }
+}
+
+export async function getAdDescription(url: string) {
+  const options = {
+    method: 'POST',
+    headers: { accept: 'application/json', 'content-type': 'application/json', authorization: `Bearer ${import.meta.env.VITE_EDEN_API_KEY}` },
+    body: JSON.stringify({
+      settings: '{"openai": "gpt-4o-mini"}',
+      response_as_dict: true,
+      attributes_as_list: false,
+      show_base_64: true,
+      show_original_response: false,
+      temperature: 0,
+      max_tokens: 200,
+      providers: ['openai'],
+      file_url: url,
+      question: "Give me a spooky but accurate description of this image in about 20-30 words"
+    })
+  };
+
+  const response = await fetch('https://api.edenai.run/v2/image/question_answer', options)  
+  const JSONresponse = await response.json()
+  console.log("Response in utils: ", JSONresponse)
+  return JSONresponse.openai.answers[0]
 }
